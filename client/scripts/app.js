8c87151e5c848c472161fb06c7eeaf97cfdcbfbb
rfigueror1 <rfigueror@gmail.com>
2018-06-25 17:09:48 -0700

    Added functionality for Movie and Movies. Tests passing.
    Still need to fix functionality for AppView, MovieView, and MoviesView

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,10 +3,28 @@ var Movie = Backbone.Model.extend({
   defaults: {
     like: true
   },
+  
+  initialize: function() {
+    // add a listener to each movie to check for change
+    // in toggleLike. If a change happens,
+    // sort the collection
+    this.on('change:like', function() {
+      this.collection.sort();
+    });
+    
+    // this.on('change', function() {
+    //   this.view.render();
+    // });
+    // 
+  },
 
   toggleLike: function() {
     // your code here
+    this.set('like', !(this.get('like')));
+    // this.sort();
+    console.log(this.on, 'model on');
   }
+  
 
 });
 
@@ -16,13 +34,36 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    // defaults: {
+    //   comparator:'title';
+    // }
+    this.comparator = 'title';
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
-  }
+    
+    this.comparator = field;
+    this.sort();
+    
+    // debugger;
+    
+    console.log(this.models, 'the collection');
+    
+    // if(this.model.hasChanged()){
+      
+    // }
+    
+    // this.model.on('change', function() {
+    //   this.collection.sort();
+    // });
+  },
+  
+  // console.log(this.model, 'this.model outside')
+  
+
 
 });
 
@@ -47,6 +88,8 @@ var AppView = Backbone.View.extend({
 });
 
 var MovieView = Backbone.View.extend({
+  
+  model: Movie,
 
   template: _.template('<div class="movie"> \
                           <div class="like"> \
